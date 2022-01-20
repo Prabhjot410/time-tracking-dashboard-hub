28cf733df4610b5db59faba609ad53b1253d639a
Prabhjot410 <58880607+Prabhjot410@users.noreply.github.com>
2022-01-20 16:48:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<​script  type="text/javascript" src ="data.json"></​script>
+
+
+document.getElementById("daily").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("daily").style.color = "wheat";}
+
+  
